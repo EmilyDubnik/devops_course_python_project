a53425234062b4731815d41a648fb0fca80a3e36
EmilyDubnik <113524004+EmilyDubnik@users.noreply.github.com>
2023-05-25 00:57:16 +0300

    Update frontend_testing.py

--- frontend_testing.py
+++ frontend_testing.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import time
 
 #Create Web driver and access frontend URL with a specific userid
 driver = webdriver.Chrome(service=Service("/Users/edubnik/Downloads/chromedriver_mac_arm64"))
 user_id = "4"
 driver.get('http://127.0.0.1:5001/users/get_user_data/' + user_id)
+time.sleep(30) 
 
 #Locate the username according the the ID locator and print the result
 user_element = driver.find_element(By.ID, "user")
